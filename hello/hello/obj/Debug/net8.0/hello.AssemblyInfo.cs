b21c391180b996f1cb873aa37362e523745a8147
Romeric15-gitlc <2736932@collegelacite.ca>
2025-12-09 14:49:51 -0500

    commit 3
    Commit 3

--- hello/hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
+++ hello/hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2066e519c700d97dd66bda50544874509003eff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
